65129cad10c9d8acced133d3373354d5cc9021f2
Aapo Kiiskilä <aapo.kiiskila@gmail.com>
2026-06-16 21:01:30 +0300

    Chapter 1-5 (Odoo tutorials)

---
+++ custom_addons/real_estate/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': "Real Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "TestUser",
+    'application': True,
+    'data': [
+      'security/ir.model.access.csv'
+    ]
+}
